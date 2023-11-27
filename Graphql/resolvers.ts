4b57bc652c1b2da10b4d1138e528ba2b4aa1d567
EL-Gazoly <elgazoly11@gmail.com>
2023-11-27 10:27:29 +0200

    add the realtion for students and users

--- Graphql/resolvers.ts
+++ Graphql/resolvers.ts
@@ -1,8 +1,74 @@
 import Query from "./Queries/Query";
 import Mutation from "./Mutaions"
+import prisma from "../config/database";
 const resolvers = {
     Query,
-    Mutation
+    Mutation,
+    User: {
+        student : async (parent : any) => {
+            return await prisma.student.findMany({
+                where: {
+                    userId: parent.id,
+                },
+            
+            })
+        },
+        devices : async (parent : any) => {
+            return await prisma.device.findMany({
+                where: {
+                    userId: parent.id,
+                },
+            
+            })
+        },
+
+      },
+    Student : {
+        user : async (parent : any) => {
+            return await prisma.user.findUnique({
+                where: {
+                    id: parent.userId,
+                },
+            
+            })
+        },
+        connectDevice : async (parent : any) => {
+            return await prisma.device.findUnique({
+                where: {
+                    deviceId: parent.deviceId,
+                },
+            
+            })
+        },
+      
+        signInOUT : async (parent : any) => {
+            return await prisma.signInOut.findMany({
+                where: {
+                    studentId: parent.id,
+                },
+            
+            })
+        },
+        studnetExpriment : async (parent : any) => {
+            return await prisma.studentExpriment.findMany({
+                where: {
+                    studentId: parent.id,
+                },
+            
+            })
+        },
+        studentCategories : async (parent : any) => {
+            return await prisma.studentCategories.findMany({
+                where: {
+                    studentId: parent.id,
+                },
+            
+            })
+        }
+
+      },
+    
+
 };
 
 export default resolvers;
--- Resolvers/User/index.ts
+++ Resolvers/User/index.ts
@@ -29,6 +29,7 @@ const userQuery = {
             },
         })
     },
+ 
 }
 
 const userMuation = {
